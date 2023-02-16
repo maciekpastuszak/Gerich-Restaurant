4b3e87bf855388f022942bbcf22ed6f1dca98498
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-02-16 12:30:23 +0100

    add html for hamburger menu for small screens

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -22,6 +22,20 @@ const Navbar = () => (
         <div />
         <a href="/" className="p__opensans">Book Table</a>
       </div>
+      <div className="app__navbar-smallscreen">
+        <GiHamburgerMenu color='#fff' fontSize={27} onClick={() => {}}/>
+        <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+          <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => {}} />
+          <ul className="app__navbar-smallscreen-links">
+            <li className="p__opensans"><a href="#home">Home</a></li>
+            <li className="p__opensans"><a href="#about">About</a></li>
+            <li className="p__opensans"><a href="#menu">Menu</a></li>
+            <li className="p__opensans"><a href="#awards">Awards</a></li>
+            <li className="p__opensans"><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+        
+      </div>
   </nav>
 );
 
