08a6db74bbfe17bc0d117a12e43a4ad12a42f377
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-02-16 12:23:45 +0100

    add navbar login-logout with styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,6 +17,11 @@ const Navbar = () => (
         <li className="p__opensans"><a href="#awards">Awards</a></li>
         <li className="p__opensans"><a href="#contact">Contact</a></li>
       </ul>
+      <div className="app__navbar-login">
+        <a href="#login" className="p__opensans">Log In / Register</a>
+        <div />
+        <a href="/" className="p__opensans">Book Table</a>
+      </div>
   </nav>
 );
 
