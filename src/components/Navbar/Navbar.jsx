725c117c4a56ace66964408ecfaf86a53c63bdbe
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-02-16 11:56:44 +0100

    prepare for navbar development

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
 
+import images from '../../constants/images';
 import './Navbar.css';
 
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+  <nav className="app__navbar">
+      Navbar
+  </nav>
 );
 
 export default Navbar;
