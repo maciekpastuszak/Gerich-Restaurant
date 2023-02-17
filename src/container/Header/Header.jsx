294927454a285c744a41258eed4026f263eabd0c
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-02-17 21:18:43 +0100

    add dynamic header title + SubHeading component

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -7,7 +7,8 @@ import './Header.css';
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
-      <SubHeading />
+      <SubHeading title="Chase the new flavour"/>
+      <h1 className="app__header-h1">The Key to Fine Dining</h1>
     </div>
 
     <div className="app__wrapper_img">
