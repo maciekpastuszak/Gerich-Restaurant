7c40d457238df01a2aa8aa445b85a1761742225a
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-02-18 17:13:56 +0100

    add right section and a knife to About us

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -14,6 +14,17 @@ const AboutUs = () => (
         <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
         <button type="button" className="custom__button">Know More</button>
       </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="about_knife" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon_img" />
+        <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
+        <button type="button" className="custom__button">Know More</button>
+      </div>
     </div>
   </div>
 );
