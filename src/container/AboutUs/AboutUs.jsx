e2c7ca5a3cb30416129c9d3343739c331e16b4cc
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-02-18 17:31:02 +0100

    add styles to history adn about containers + knife

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -4,8 +4,8 @@ import './AboutUs.css';
 
 const AboutUs = () => (
   <div className="app__aboutus app__bg flex__center section__padding" id="about">
-    <div className="app__aboutus-overlay flex-center">
-      <img src={images.G} alt="g letter" />
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="g_letter" />
     </div>
     <div className="app__aboutus-content flex__center">
       <div className="app__aboutus-content_about">
@@ -15,7 +15,7 @@ const AboutUs = () => (
         <button type="button" className="custom__button">Know More</button>
       </div>
 
-      <div className="app__aboutus-content_knife flex__center">
+      <div className="app__aboutus-content_knife flex_center">
         <img src={images.knife} alt="about_knife" />
       </div>
 
