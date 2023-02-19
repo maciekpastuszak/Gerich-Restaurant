e93d711f250163f99d23dedaffdc6c2f84ad07a3
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-02-19 16:36:03 +0100

    add SpecialMenu basic structure

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
-
+import { images, data } from '../../constants';
+import { SubHeading, MenuItem } from '../../components';
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className="app__specialMenu flex__center section __padding" id="menu">
+    <div className="app__specialMenu-title">
+      <SubHeading title="Menu that fits your palette" />
+        <h1 className="headtext__cormorant">Today's Special</h1>
+    </div>
+
+    <div className="app__specialMenu-menu">
+      <div className="app__specialMenu-menu_wine flex__center">
+        <p className="app__specialMenu-menu_heading">Wine & Beer</p>
+        <div className="app__specialMenu-menu items">
+          {data.wines.map((wine, index) => (
+            <p>{wine.title}</p>
+          ))}
+        </div>
+      </div>
+    </div>
   </div>
 );
 
