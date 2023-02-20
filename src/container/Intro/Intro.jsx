b85f4d1aac9c97c0f7b82cd346f874c5c06dbc7d
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-02-20 22:41:22 +0100

    add handleVideo onClick event

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -5,20 +5,37 @@ import { meal } from '../../constants';
 import './Intro.css';
 
 const Intro = () => {
-  const [playVideo, setplayVideo] = useState(false);
+  const [playVideo, setPlayVideo] = useState(false);
   const vidRef = React.useRef();
+
+  const handleVideo = () => {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo);
+    if(playVideo) {
+      vidRef.current.pause();
+    } else {
+      vidRef.current.play();
+    }
+  
+  }
   
   return (
     <div className="app__video">
-    <video 
-      src={meal}
-      ref={vidRef}
-      type="video/mp4"
-      Loop 
-      controls={false}
-      muted
-    />
-  </div>
+      <video 
+        src={meal}
+        ref={vidRef}
+        type="video/mp4"
+        Loop 
+        controls={false}
+        muted
+      />
+      <div className="app__video-overlay flex__center">
+        <div className="app__video-overlay_circle flex__center"
+        onClick={handleVideo}
+        >
+          
+        </div>
+      </div>
+    </div>
   )
 };
 
