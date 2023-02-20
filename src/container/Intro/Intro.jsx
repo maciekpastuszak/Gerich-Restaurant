23d3b3d5b2044d379db6b8b06564ba7cd82eac38
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-02-20 22:35:46 +0100

    initialize Intro component

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
 
+import { meal } from '../../constants';
 import './Intro.css';
 
-const Intro = () => (
-  <div>
-    Intro
+const Intro = () => {
+  const [playVideo, setplayVideo] = useState(false);
+  const vidRef = React.useRef();
+  
+  return (
+    <div className="app__video">
+    <video 
+      src={meal}
+      ref={vidRef}
+      type="video/mp4"
+      Loop 
+      controls={false}
+      muted
+    />
   </div>
-);
+  )
+};
 
 export default Intro;
