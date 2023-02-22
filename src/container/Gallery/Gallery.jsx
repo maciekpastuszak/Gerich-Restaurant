edc56a23709285fb97350e1af8899c15851765ca
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-02-22 21:04:02 +0100

    add gallery img arrows and gallery img to Gallery container

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -7,10 +7,25 @@ import './Gallery.css';
 
 const Gallery = () => {
  
+  const scrollRef = React.useRef(null);
+
  return (
     <div className="app__gallery flex__center">
         <div className="app__gallery-content">
           <SubHeading title="Instagram" />
+          <h1 className="headtext__cormorant">Photo Gallery</h1>
+          <p className="p__opensans" style={{color: '#AAA', marginTop: '2rem'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
+          <button type="button" className="custom__button">View More</button>
+        </div>
+
+        <div className="app__gallery-images">
+          <div className="app__gallery-images_container" ref={scrollRef}>
+
+          </div>
+          <div className="app__gallery-images_arrow">
+              <BsArrowLeftShort className="gallert_arrow-icon" onClick={() => scroll('left')} />
+              <BsArrowRightShort className="gallert_arrow-icon" onClick={() => scroll('right')} />
+          </div>
         </div>
     </div>
  )
