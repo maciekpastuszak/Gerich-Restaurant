a3c1d9a1a19be34e9ce371372ec4303937ee8e38
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-02-22 20:55:29 +0100

    implement gallery initial html and style

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import {BsInstagram, BsArrowLeftShort, BsArrowRightShort} from 'react-icons/bs';
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 
 import './Gallery.css';
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
+const Gallery = () => {
+ 
+ return (
+    <div className="app__gallery flex__center">
+        <div className="app__gallery-content">
+          <SubHeading title="Instagram" />
+        </div>
+    </div>
+ )
+
+};
 
 export default Gallery;
