4f5b67fa818b221932fc7187248a4bd4f47f0355
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-02-22 21:13:27 +0100

    add images gallery to Gallery container

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -5,6 +5,8 @@ import { images } from '../../constants';
 
 import './Gallery.css';
 
+const galleryImages = [images.gallery01, images.gallery02, images.gallery03, images.gallery04,];
+
 const Gallery = () => {
  
   const scrollRef = React.useRef(null);
@@ -30,7 +32,12 @@ const Gallery = () => {
 
         <div className="app__gallery-images">
           <div className="app__gallery-images_container" ref={scrollRef}>
-
+            {galleryImages.map((image, index) => (
+              <div className="app__gallery-images_card flex__center" key={`gallery_image-${index + 1}`}>
+                <img src={image} alt="gallery" />
+                <BsInstagram className="gallery__image-icon" />
+              </div>
+            ))}
           </div>
           <div className="app__gallery-images_arrow">
               <BsArrowLeftShort className="gallert_arrow-icon" onClick={() => scroll('left')} />
