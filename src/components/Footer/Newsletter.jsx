3ed9a017fe01632f066ca634377482fec42b8fd0
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-02-23 23:09:47 +0100

    add content to Newsletter compnent

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
 import './Newsletter.css';
+import SubHedaing from '../Subheading/Subheading';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className="app__newsleter">
+    <div className="app__newsleter-heading">
+      <SubHeading title="Newsletter" />
+      <h1 className="headtext__cormorant">Subscribe To Our Newsletter</h1>
+      <p className="p__opensans">And never miss latest updates!</p>
+    </div>
   </div>
 );
 
