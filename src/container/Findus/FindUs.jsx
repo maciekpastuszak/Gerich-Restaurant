e1ef099afabd5dc67092dbdde69b37f12812221c
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-02-23 16:06:11 +0100

    add Find Us section

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+
 
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className="app__bg app__wrapper section__padding" id="contact">
+    <div className="app__wrapper_info">
+      <SubHeading title="Contact" />
+      <h1 className="headtext__cormorant" style={{marginBottom: '3rem'}}>Find Us</h1>
+      <div className="app__wrapper-content">
+        <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
+        <p className="p__cormorant" style={{color: '#DCCA87', margin: '2rem 0'}}>Opening Hours</p>
+        <p className="p__opensans">Monday-Friday:
+08:00 am -12:00 am</p>
+        <p className="p__opensans">Saturday-Sunday:
+07:00am -11:00 pm</p>
+      </div>
+      <button className="custom__button" style={{marginTop: '2rem'}}>Visit Us</button>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.findus} alt="find_us" />
+    </div>
   </div>
 );
 
