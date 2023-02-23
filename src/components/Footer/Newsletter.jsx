d697edbfd2cd32293ff067a6e79844be78868961
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-02-23 23:16:16 +0100

    bugfixes - typos

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import './Newsletter.css';
-import SubHedaing from '../Subheading/Subheading';
+import SubHeading from '../SubHeading/SubHeading';
 
 const Newsletter = () => (
-  <div className="app__newsleter">
-    <div className="app__newsleter-heading">
+  <div className="app__newsletter">
+    <div className="app__newsletter-heading">
       <SubHeading title="Newsletter" />
       <h1 className="headtext__cormorant">Subscribe To Our Newsletter</h1>
       <p className="p__opensans">And never miss latest updates!</p>
