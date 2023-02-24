da6f795ef68d9e9798d64958168f78516e5ac06e
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-02-24 09:08:59 +0100

    add footer left column

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -2,12 +2,31 @@ import React from 'react';
 import {FiFacebook, FiTwitter, FiInstagram} from 'react-icons/fi';
 
 import { FooterOverlay, Newsletter} from '../../components';
+import { images } from '../../constants'
 import './Footer.css';
 
 const Footer = () => (
   <div className="app__footer section__padding">
     <FooterOverlay />
     <Newsletter />
+
+    <div className="app__footer-inks">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+        <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
+        <p className="p__opensans">+1 212-344-1230</p>
+        <p className="p__opensans">+1 212-555-1230</p>
+      </div>
+      <div className="app__footer-links_logo">
+        
+      </div>
+      <div className="app__footer-links_work">
+
+      </div>
+    </div>
+    <div className="footer__copyright">
+      <p className="p__opensans">2023 Gerícht. All Rights reserved.</p>
+    </div>
   </div>
 );
 
