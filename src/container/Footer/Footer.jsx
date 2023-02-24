1693d7596a5762a5239a1ac6f28b7cb186b95962
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-02-24 09:15:24 +0100

    add middle and right column to footer

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -18,10 +18,21 @@ const Footer = () => (
         <p className="p__opensans">+1 212-555-1230</p>
       </div>
       <div className="app__footer-links_logo">
-        
+        <img src={images.gericht} alt="footer_logo" />
+        <p className="p__opensans">"The best way to find yourself is to lose yourself in the service of others.”</p>
+        <img src={images.spoon} alt="spoon" className="spoon_img" style={{margintop: 15}} />
+        <div className="app__footer-links_icons">
+          <FiFacebook />
+          <FiTwitter />
+          <FiInstagram />
+        </div>
       </div>
       <div className="app__footer-links_work">
-
+        <h1 className="app__footer-headtext">Working Hours</h1>
+        <p className="p__opensans">Monday-Friday:</p>
+        <p className="p__opensans">08:00 am - 12:00 am</p>
+        <p className="p__opensans">Saturday-Sunday:</p>  
+        <p className="p__opensans">07:00 am - 11:00 pm</p>  
       </div>
     </div>
     <div className="footer__copyright">
