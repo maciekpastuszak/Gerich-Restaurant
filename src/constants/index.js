a90d65284d2b6aee7d6885c6e1801819d8707989
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2025-09-20 00:19:38 +0200

    update video path

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
-import images from './images';
-import meal from '../assets/meal.mp4';
-import data from './data';
+import images from "./images";
+import meal from "/videos/meal.mp4";
+import data from "./data";
 
 export { images, meal, data };
--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
+import React, { useState } from "react";
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
 
-import { meal } from '../../constants';
-import './Intro.css';
+import { meal } from "../../constants";
+import "./Intro.css";
 
 const Intro = () => {
   const [playVideo, setPlayVideo] = useState(false);
@@ -10,35 +10,37 @@ const Intro = () => {
 
   const handleVideo = () => {
     setPlayVideo((prevPlayVideo) => !prevPlayVideo);
-    if(playVideo) {
+    if (playVideo) {
       vidRef.current.pause();
     } else {
       vidRef.current.play();
     }
-  
-  }
-  
+  };
+
   return (
     <div className="app__video">
-      <video 
-        src={meal}
+      <video
+        src="/videos/meal.mp4"
         ref={vidRef}
         type="video/mp4"
-        loop 
+        loop
         controls={false}
         muted
       />
       <div className="app__video-overlay flex__center">
-        <div className="app__video-overlay_circle flex__center"
-        onClick={handleVideo}
+        <div
+          className="app__video-overlay_circle flex__center"
+          onClick={handleVideo}
         >
           {playVideo ? (
-            <BsPauseFill color="#fff" fontSize={30}/>
-          ) : <BsFillPlayFill color="#fff" fontSize={30}/>}
+            <BsPauseFill color="#fff" fontSize={30} />
+          ) : (
+            <BsFillPlayFill color="#fff" fontSize={30} />
+          )}
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Intro;
